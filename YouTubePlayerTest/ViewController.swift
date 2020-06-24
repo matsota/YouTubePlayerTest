499383bc223fa0a089016a98c0a917ba60b08c43
Matsota Andrew <matsotaandrew@icloud.com>
2020-06-24 17:40:39 +0300

    Initial Commit

---
+++ YouTubePlayerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubePlayerTest
+//
+//  Created by Andrew Matsota on 24.06.2020.
+//  Copyright © 2020 Andrew Matsota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
